bedc3ceba482e60eb9cfd8c613012088551ce4e9
Paul Schmitt <pschmitt@oncorhynchus-mykiss>
2020-03-30 16:08:36 -0700

    NM worker module

--- core/modules/netmicroscope.cc
+++ core/modules/netmicroscope.cc
@@ -1,32 +1,16 @@
-/* BESS version based on cryptopANT 1.2.1 (2019-09-30)
- * Modified by Paul Schmitt
- * Copyright (C) 2004-2019 by the University of Southern California
- * $Id: 89d8a3f3fea9f54bc16a49c7c9d8788716f83f8f $
- *
- * This program is free software; you can redistribute it and/or
- * modify it under the terms of the GNU General Public License,
- * version 2, as published by the Free Software Foundation.
- *
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
- *
- * You should have received a copy of the GNU General Public License along
- * with this program; if not, write to the Free Software Foundation, Inc.,
- * 59 Temple Place, Suite 330, Boston, MA 02111-1307, USA.
- *
- */
-
 #include "netmicroscope.h"
+#include "../utils/cuckoo_map.h"
+#include "nm_stats.h"
 
 #include "../utils/ether.h"
 #include "../utils/ip.h"
 #include "../utils/udp.h"
 using bess::utils::be32_t;
+using bess::utils::NM_Flowcache;
 using bess::utils::ToIpv4Address;
 
-netmicroscope::FlowTuple netmicroscope::GetTuple(bess::Packet *pkt) {
+
+NM_Flowcache::FlowTuple GetTuple(bess::Packet *pkt) {
   using bess::utils::Ethernet;
   using bess::utils::Ipv4;
   using bess::utils::Udp;
@@ -37,8 +21,9 @@ netmicroscope::FlowTuple netmicroscope::GetTuple(bess::Packet *pkt) {
   Udp *udp = reinterpret_cast<Udp *>(reinterpret_cast<uint8_t *>(ip) +
                                      ip_bytes);  // Assumes a l-4 header
   // TODO: handle packet fragmentation
-  FlowTuple ft = {ip->src.value(), ip->dst.value(), udp->src_port.value(),
-                  udp->dst_port.value(), ip->protocol};
+  NM_Flowcache::FlowTuple ft = {ip->src.value(), ip->dst.value(),
+                                udp->src_port.value(), udp->dst_port.value(),
+                                ip->protocol};
   return ft;
 }
 
@@ -51,24 +36,19 @@ void netmicroscope::ProcessBatch(Context *ctx, bess::PacketBatch *batch) {
     using bess::utils::Ipv4;
     using bess::utils::Udp;
 
-    FlowTuple ft = GetTuple(pkt);
+    NM_Flowcache::FlowTuple ft = GetTuple(pkt);
 
-    auto it = flowcache.find(ft);
+    auto it = NMFC.flowcache.Find(ft);
 
-    if (it == flowcache.end()) {
-      Flow *f = new Flow(ft);
+    if (it == nullptr) {
+      NM_Flowcache::Flow *f = new NM_Flowcache::Flow(ft);
       f->pkts += 1;
-      flowcache.insert(std::make_pair(ft, f));
+      NMFC.flowcache.Insert(ft, f);
     } else {
       it->second->pkts += 1;
     }
   }
-  for (auto ii = flowcache.begin(); ii != flowcache.end(); ii++)
-    printf("Cache: %s %s %d %d %d\n",
-           ToIpv4Address(be32_t(ii->first.src_ip)).c_str(),
-           ToIpv4Address(be32_t(ii->first.dst_ip)).c_str(), ii->first.src_port,
-           ii->first.dst_port, ii->second->pkts);
-  printf("=================\n");
+
   RunNextModule(ctx, batch);
 }
 
--- core/modules/netmicroscope.h
+++ core/modules/netmicroscope.h
@@ -29,93 +29,11 @@
 
 #include "../module.h"
 
-uint64_t fnv_basis = 14695981039346656037u;
-uint64_t fnv_prime = 1099511628211u;
-
 class netmicroscope final : public Module {
  public:
-  // 5 tuple id to identify a flow from a packet header information.
-  struct FlowTuple {
-    uint32_t src_ip;
-    uint32_t dst_ip;
-    uint16_t src_port;
-    uint16_t dst_port;
-    uint8_t protocol;
-    bool operator==(const FlowTuple &other) const {
-      return (this->src_ip == other.src_ip && this->dst_ip == other.dst_ip &&
-              this->src_port == other.src_port &&
-              this->dst_port == other.dst_port &&
-              this->protocol == other.protocol) ||
-             (this->src_ip == other.dst_ip && this->dst_ip == other.src_ip &&
-              this->src_port == other.dst_port &&
-              this->dst_port == other.src_port &&
-              this->protocol == other.protocol);
-    }
-  };
-
-  class Flow {
-   public:
-    int pkts;  // packet counter
-    int pktsup;
-    int pktsdown;
-    FlowTuple ft;
-
-    Flow(FlowTuple new_tuple)
-        : pkts(0), pktsup(0), pktsdown(0), ft(new_tuple){};
-  };
-
-  // hashes a FlowTuple
-  struct Hash {
-    // a similar method to boost's hash_combine in order to combine hashes
-    inline void combine(std::size_t &hash, const unsigned int &val) const {
-      std::hash<unsigned int> hasher;
-      hash ^= hasher(val) + 0x9e3779b9 + (hash << 6) + (hash >> 2);
-    }
-    size_t operator()(const FlowTuple &ft) const {
-      // bess::utils::HashResult operator()(const FlowTuple &ft) const {
-      std::size_t hash = 0;
-      combine(hash, ft.src_ip);
-      combine(hash, ft.dst_ip);
-      combine(hash, ft.src_port);
-      combine(hash, ft.dst_port);
-      combine(hash, (uint32_t)ft.protocol);
-      return hash;
-    }
-  };
-  struct fastHash {
-    size_t operator()(const FlowTuple &ft) const {
-      std::size_t hash = 0;
-      hash ^= ft.src_ip;
-      hash ^= ft.dst_ip;
-      hash ^= ft.src_port;
-      hash ^= ft.dst_port;
-      hash ^= ft.protocol;
-      return hash;
-    }
-  };
-
-  struct fastHash2 {
-    uint64_t operator()(const FlowTuple &ft) const {
-      std::size_t src = std::hash<uint32_t>()(ft.src_ip);
-      std::size_t dst = std::hash<uint32_t>()(ft.dst_ip);
-      std::size_t sport = std::hash<uint16_t>()(ft.src_port);
-      std::size_t dport = std::hash<uint16_t>()(ft.dst_port);
-      std::size_t proto = std::hash<uint8_t>()(ft.protocol);
-
-      return src ^ dst ^ sport ^ dport ^ proto;
-    }
-  };
-
-  std::unordered_map<FlowTuple, Flow *, fastHash2> flowcache;
-
   netmicroscope() : Module() { max_allowed_workers_ = Worker::kMaxWorkers; }
 
   void ProcessBatch(Context *ctx, bess::PacketBatch *batch) override;
-
- private:
-  //  Takes a Packet to get a flow id for. Returns the 5 element identifier
-  //  for the flow that the packet belongs to
-  FlowTuple GetTuple(bess::Packet *pkt);
 };
 
 #endif  // BESS_MODULES_NETMICROSCOPE_H_
